58bf83172758533b2d48e9bce30da81d52fb21c6
sarthak Gokhale <sarthakgokhale15@gmail.com>
2021-11-28 14:42:41 +0530

    First Commit

---
+++ Templates/binaryExpo.cpp
@@ -0,0 +1,17 @@
+// find a^b in (logn) time
+// if numbers are too big then return ans%p
+int powermod(int x, int y, int p)
+{
+    int res = 1;
+    x = x % p;
+    if (x == 0)
+        return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
