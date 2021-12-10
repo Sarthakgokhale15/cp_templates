b8b76f6593fb112f332e2c3056bfab3ab29809af
sarthak Gokhale <sarthakgokhale15@gmail.com>
2021-12-10 20:52:54 +0530

    10 DEC

--- Templates/slide_wind_k.cpp
+++ Templates/slide_wind_k.cpp
@@ -1,5 +1,5 @@
 
-int slide_wind_k(vi v,int k,int n){
+int slide_wind_k_max(vi v,int k,int n){
    int max_sum=0;
    int wind_sum=0;
    //find sum of first window from 0-k
@@ -15,5 +15,24 @@ max_sum=max(max_sum,wind_sum);
 }
 
 return max_sum;
+
+}
+
+int slide_wind_k_min(vi v,int k,int n){
+   int min_sum=INT_MAX;
+   int wind_sum=0;
+   //find sum of first window from 0-k
+   rep(i,k){
+wind_sum+=v[i];
+   }
+max_sum=min(min_sum,wind_sum);
+
+
+for(int i=k;i<n;i++){
+      wind_sum+=(v[i]-v[i-k]);
+min_sum=min(min_sum,wind_sum);
+}
+
+return min_sum;
 
 }
