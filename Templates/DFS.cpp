bdb9e698f5c6aeaa954849c43e431b1bf9f1fc7a
VIPER_619 <64783930+Sarthakgokhale15@users.noreply.github.com>
2022-03-13 17:07:23 +0530

    update DFS.cpp
    updated more general DFS

--- Templates/DFS.cpp
+++ Templates/DFS.cpp
@@ -11,3 +11,20 @@ void dfs(vector<int> &ans, vector<int> adj[], map<int, bool> &visited, int src)
         dfs(ans, adj, visited, u);
     }
 }
+
+##############GENERAL DFS################ 
+
+
+void dfs(vector<vector<int> > &grid, vector<vector<int> > &visited, int i, int j, int n, int m)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m || visited[i][j] == 1 || grid[i][j] == 0)
+    {
+        return;
+    }
+
+    visited[i][j] = 1;
+    dfs(grid, visited, i - 1, j, n, m);
+    dfs(grid, visited, i + 1, j, n, m);
+    dfs(grid, visited, i, j - 1, n, m);
+    dfs(grid, visited, i, j + 1, n, m);
+}
